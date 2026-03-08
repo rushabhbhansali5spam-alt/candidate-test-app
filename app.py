43c2e9ebb3285006b26c820b8b8b15ffd0e4a82d
rushabhbhansali5spam-alt <rushabhbhansali5spam@gmail.com>
2026-03-08 16:06:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from datetime import datetime
 
 # Google setup
 scope = [
+"https://spreadsheets.google.com/feeds",
 "https://www.googleapis.com/auth/spreadsheets",
+"https://www.googleapis.com/auth/drive.file",
 "https://www.googleapis.com/auth/drive"
 ]
 
